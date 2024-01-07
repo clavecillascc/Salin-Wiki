d2ac2aebf4d8880599d00976ba57cdb12ae2e763
clavecillascc <clavecillascc@gmail.com>
2024-01-07 14:00:19 +0000

    fixed database.py

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine, text
 
 engine = create_engine(
-    "mysql+pymysql://ccfs3wxctgjs7lftkian:pscale_pw_bXvR0fDEVyYhT82aUVfcS9BpLJPAOXYLfIlqVloCKbp@aws.connect.psdb.cloud/salinwiki?charset=utf8mb4",
+    "mysql+pymysql://hg8gg9giycncjka1od68:pscale_pw_GNqlipCAzQ151g8U7QS92u2bJZGbNpYbJjXbvTjsPet@aws.connect.psdb.cloud/salinwiki?charset=utf8mb4",
     connect_args={"ssl": {
         "ssl_cert": "/etc/ssl/cert.pem"
     }})
+
 
 def load_words_from_db():
   with engine.connect() as conn:
